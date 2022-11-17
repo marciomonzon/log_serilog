144a6cca2dcada7f54a9631a6a98c2ad0ca38a68
marciomonzon <marcio.pcmonzon@gmail.com>
2022-11-16 22:26:52 -0300

    frist commit

---
+++ SeriLog_Demo/SeriLog_Demo/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace SeriLog_Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            _logger.LogInformation("Endpoint Get successful. (from ILogger)");
+            Log.Information("Endpoint Get successful. (from Serilog)");
+
+            return Ok();
+        }
+    }
+}
